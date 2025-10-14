50fb64bab09911a76f244eb0426880855ef55f31
priteshkoshiya <koshiyapritesh21@gmail.com>
2025-10-14 15:34:45 +0530

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-import { tanstackRouter } from '@tanstack/router-plugin/vite'
-
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackRouter } from '@tanstack/router-plugin/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,22 +13,24 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  resolve: {  
+  resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-
-      // fix loading all icon chunks in dev mode
-      // https://github.com/tabler/tabler-icons/issues/1233
       '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
     },
   },
-    server: {
-  proxy: {
-    '/api': {
-      target: 'http://localhost:3000/v1',  
-      changeOrigin: true,
-      secure: false,
+  server: {
+    host: '0.0.0.0',  // Add this line
+    port: 5173,        // Optional: explicitly set port
+    allowedHosts: [
+      'http://192.168.10.46:5173/', // Allows a specific host
+    ],
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/v1',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
-}   
-})
+});
